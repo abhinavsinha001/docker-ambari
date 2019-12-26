f61c0d5646b17a1e9ec8c2ce3e8328b243f9e0bc
abhinav-sinha <abhinav.sinha@pubmatic.com>
2019-12-27 01:45:11 +0530

    Using StatefulSet

--- kubernetes/startagent.sh
+++ kubernetes/startagent.sh
@@ -1,3 +1,5 @@
 #bin/bash
 tail -F /var/log/ambari-agent/ambari-agent.log &
+sed -i 's/verify=platform_default/verify=disable/' /etc/python/cert-verification.cfg
+sed -i "s,^hostname=.*$,hostname=$AMBARI_SERVER," ambari-agent.ini
 ambari-agent start
