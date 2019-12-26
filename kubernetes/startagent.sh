1b9bb006c14b71d59a8ebc7f0115d73f85d8cf59
abhinav-sinha <abhinav.sinha@pubmatic.com>
2019-12-27 01:55:11 +0530

    Fixed config path

--- kubernetes/startagent.sh
+++ kubernetes/startagent.sh
@@ -1,5 +1,5 @@
 #bin/bash
 tail -F /var/log/ambari-agent/ambari-agent.log &
 sed -i 's/verify=platform_default/verify=disable/' /etc/python/cert-verification.cfg
-sed -i "s,^hostname=.*$,hostname=$AMBARI_SERVER," ambari-agent.ini
+sed -i "s,^hostname=.*$,hostname=$AMBARI_SERVER," /etc/ambari-agent/conf/ambari-agent.ini
 ambari-agent start
